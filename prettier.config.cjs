35ce06f238acedf1fca736305ee701629699a40d
gearintellix <luthfigearintellix@gmail.com>
2024-08-19 23:52:03 +0700

    feat(prettier): add prettier config to unified formatting

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+}
+
+module.exports = config
